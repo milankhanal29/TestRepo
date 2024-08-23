f4e70af6b6b673e78e5c6b245a025bbf65cd019c
milankhanal29 <meelonkhanal2@gmail.com>
2024-08-23 07:48:04 +0545

    test commit

---
+++ aa.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Map;
+
+public class aa {
+
+    @PostMapping(value = "/github/{email}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> handleGitHubWebhook(
+            @PathVariable String email,
+            @RequestBody Map<String, Object> payload) throws IOException {
+        System.out.println("Received GitHub webhook with payload: " + payload);
+        webhookService.processWebhook(payload, email);
+        return new ResponseEntity<>("Webhook received", HttpStatus.OK);
+    }
+}
