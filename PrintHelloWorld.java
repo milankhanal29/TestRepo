1f5bc75ae1d11e0b912195554b3970f671765df3
milankhanal29 <meelonkhanal2@gmail.com>
2024-09-22 10:01:48 +0545

    test 22

--- PrintHelloWorld.java
+++ PrintHelloWorld.java
@@ -1,18 +1,25 @@
 public class PrintHelloWorld {
+    public String encrypt(String text) {
+        StringBuilder rail1 = new StringBuilder();
+        StringBuilder rail2 = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            if (i % 2 == 0) {
+                rail1.append(text.charAt(i));
+            } else {
+                rail2.append(text.charAt(i));
+            }
+        }
+        return rail1.toString() + rail2.toString();
+    }
 
     public static void main(String[] args) {
-        printMessage();
-    }
-    }
-    public static void main(String[] args) {
-        printMessage();
-    }
-    }
+        PrintHelloWorld railfence = new PrintHelloWorld();
 
+        String plainText = "UNIVERSITY";
 
-    public static void main(String[] args) {
-        printMessage();
-    }
-    }
+        String cipherText = railfence.encrypt(plainText.toUpperCase());
 
+        System.out.println("Cipher Text: " + cipherText);
+    }
+}
 
