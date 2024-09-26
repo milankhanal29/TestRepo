53a2e2e5affcf99c0e34b8c66f96cf42769affb8
milan <meelonkhanal2@gmail,com>
2024-09-26 23:07:13 +0545

    test

--- PrintHelloWorld.java
+++ PrintHelloWorld.java
@@ -1,8 +1,6 @@
 public class PrintHelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello world");
     }
     }
 
